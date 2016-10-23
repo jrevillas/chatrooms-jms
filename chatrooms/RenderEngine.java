f11082e8a187fc28735dfd67435ad7e4999cc4e9
Javier Revillas <j.revillas@alumnos.upm.es>
2016-10-24 01:37:14 +0200

    Introducing messages

--- chatrooms/RenderEngine.java
+++ chatrooms/RenderEngine.java
@@ -1,6 +1,8 @@
 package chatrooms;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by jrevillas on 21/10/2016.
@@ -12,20 +14,20 @@ public class RenderEngine {
     private static int currentTopic;
     private static final String CYAN = "\u001B[36m";
     private static final String GREEN = "\u001B[32m";
-    private static ArrayList messages;
+    private static List messages;
     private static final String PURPLE = "\u001B[35m";
     private static final String RED = "\u001B[31m";
     private static final String RESET = "\u001B[0m";
     private static String[] scene;
     private static final int SCENE_HEIGHT = 30;
     private static final int SCENE_WIDTH = 120;
-    private static ArrayList topics;
+    private static List topics;
     private static final String WHITE = "\u001B[37m";
     private static final String YELLOW = "\u001B[33m";
 
     static {
         currentTopic = 0;
-        messages = new ArrayList<String>();
+        messages = new LinkedList<String>();
         scene = new String[SCENE_HEIGHT - 1];
         topics = new ArrayList<String>();
 
@@ -38,7 +40,7 @@ public class RenderEngine {
         return currentTopic;
     }
 
-    public static ArrayList getMessages() {
+    public static List getMessages() {
         return messages;
     }
 
@@ -46,7 +48,7 @@ public class RenderEngine {
         return scene;
     }
 
-    public static ArrayList getTopics() {
+    public static List getTopics() {
         return topics;
     }
 
@@ -70,8 +72,16 @@ public class RenderEngine {
         return String.format("%1$" + padding + "s", str);
     }
 
+    public static void addMessage(String msg) {
+        if (messages.size() > SCENE_HEIGHT - 4) {
+            messages.remove(0);
+        }
+        messages.add(msg);
+    }
+
     public static void render() {
         renderTopics();
+        renderMessages();
 
         StringBuilder str = new StringBuilder();
         for (String line : scene) {
@@ -80,12 +90,22 @@ public class RenderEngine {
         System.out.print(str.toString());
     }
 
+    private static void renderMessages() {
+        for (int i = 0; i < messages.size(); i++) {
+            scene[i + 1] += "   " + messages.get(i);
+        }
+    }
+
     private static void renderTopics() {
         scene[1] = " ┌ #" + rightPad(topics.get(0).toString(), 15) + " [ ] ┐";
         for (int i = 1; i < topics.size() - 1; i++) {
             scene[i + 1] = " │ #" + rightPad(topics.get(i).toString(), 15) + " [ ] │";
         }
         scene[topics.size()] = " └ #" + rightPad(topics.get(topics.size() - 1).toString(), 15) + " [ ] ┘";
+
+        for (int i = 1; i < scene.length - topics.size(); i++) {
+            scene[topics.size() + i] = rightPad("", 25);
+        }
     }
 
     private static String rightPad(String str, int padding) {
--- chatrooms/RenderEngineTest.java
+++ chatrooms/RenderEngineTest.java
@@ -1,17 +1,46 @@
 package chatrooms;
 
+import java.util.Scanner;
+
 /**
  * Created by jrevillas on 21/10/2016.
  */
 public class RenderEngineTest {
 
+    private static final String BLACK = "\u001B[30m";
+    private static final String BLUE = "\u001B[34m";
+    private static final String CYAN = "\u001B[36m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String PURPLE = "\u001B[35m";
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+    private static final String WHITE = "\u001B[37m";
+    private static final String YELLOW = "\u001B[33m";
+
     public static void main(String[] args) {
+        RenderEngine.addMessage("[@" + BLUE + "dmelero" + RESET + "] This is a test message.");
+        RenderEngine.addMessage("[@" + CYAN + "jruiz" + RESET + "] This is another test message.");
+        RenderEngine.addMessage("[@" + GREEN + "mnunez" + RESET + "] This is just another test message.");
+
         RenderEngine.getTopics().add("general");
         RenderEngine.getTopics().add("consejo-datsi");
         RenderEngine.getTopics().add("consejo-dia");
         RenderEngine.getTopics().add("consejo-dlsis");
         RenderEngine.getTopics().add("consejo-dmatic");
+
         RenderEngine.render();
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(" > ");
+            String input = scanner.nextLine();
+            if (input.startsWith("newtopic")) {
+                RenderEngine.getTopics().add(input.substring(input.lastIndexOf(" ") + 1));
+            } else {
+                RenderEngine.addMessage("[@" + PURPLE + "jrevillas" + RESET + "] " + input);
+            }
+            RenderEngine.render();
+        }
     }
 
 }
